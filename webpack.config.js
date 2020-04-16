6d1a29a3b18fa9d7800c97a6fc917592c94f83c1
Jack <->
2020-04-16 21:43:39 +0200

    Fix name

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,6 @@ module.exports = {
     entry: "./src/index.js",
     output: {
         path: path.resolve(__dirname, "dist"),
-        filename: "webpack-numbers.js"
+        filename: "viewbind.js"
     }
 };
