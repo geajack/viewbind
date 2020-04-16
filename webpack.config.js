50ae541003ad2a2073a27e0ac69217b55b85ea3e
Jack <->
2020-04-16 21:58:49 +0200

    update

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ module.exports = {
     entry: "./src/index.js",
     output: {
         path: path.resolve(__dirname, "dist"),
-        filename: "viewbind.js"
+        filename: "viewbind.js",
+        library: "viewbind"
     }
 };
